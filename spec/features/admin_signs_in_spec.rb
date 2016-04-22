b5eaaa16adf57f779dceb083791f933a1de4fea9
Anthony DeGennaro <anthonydegennaro@Anthonys-MacBook.local>
2016-04-22 09:34:13 -0400

    added admin to users

---
+++ spec/features/admin_signs_in_spec.rb
@@ -0,0 +1,16 @@
+require "rails_helper"
+
+feature "admin signs in" do
+  scenario "existing user specifies valid email and password" do
+    admin1 = FactoryGirl.create(:user, admin: true)
+    visit root_path
+    click_link "Sign In"
+    fill_in "Email", with: admin1.email
+    fill_in "Password", with: admin1.password
+    click_button "Sign In"
+
+    expect(page).to have_content("Welcome Back!")
+    expect(page).to have_content("Sign Out")
+    expect(admin1.admin).to eq true
+  end
+end
