75e58c9d602c26f6dc50642f7264823c90daf632
Anthony DeGennaro <anthonydegennaro@Anthonys-MacBook.local>
2016-04-22 11:52:17 -0400

    game confirmation controllers and models

---
+++ db/migrate/20160422153353_create_games.rb
@@ -0,0 +1,13 @@
+class CreateGames < ActiveRecord::Migration
+  def change
+    create_table :games do |t|
+      t.string :street, null: false
+      t.string :city, null: false
+      t.string :state, null: false
+      t.integer :zip, null: false
+      t.date :game_day, null: false
+      t.time :game_time, null: false
+      t.string :opponent
+    end
+  end
+end
