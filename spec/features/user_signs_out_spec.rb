4e726ad5c3c4da474f5d040356bdb93f559d3a8e
Anthony Degennaro <anthonydegennaro@Anthonys-iMac.fios-router.home>
2016-04-21 22:37:38 -0400

    fixed sign out final

---
+++ spec/features/user_signs_out_spec.rb
@@ -0,0 +1,16 @@
+require "rails_helper"
+
+feature "user signs out" do
+  scenario "authenticated user successfully signs out" do
+    user = FactoryGirl.create(:user)
+    visit root_path
+    click_link "Sign In"
+    fill_in "Email", with: user.email
+    fill_in "Password", with: user.password
+    click_button "Sign In"
+    click_link "Sign Out"
+
+    expect(page).to have_content("See Ya!")
+    expect(page).to have_content("Sign In")
+  end
+end
