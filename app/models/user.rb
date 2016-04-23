08c0733cdb764a7869d9a0745bd105e348b95baa
Anthony DeGennaro <anthonydegennaro@AnthonysMacBook.fios-router.home>
2016-04-22 20:27:02 -0400

    fixed hound issues

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ActiveRecord::Base
   has_many :confirmations
-  
   validates_presence_of :first_name
   validates_presence_of :last_name
   devise :database_authenticatable, :registerable,
