df137b05a4a237cc008e5d283823e8783943a158
Anthony DeGennaro <anthonydegennaro@AnthonysMacBook.fios-router.home>
2016-04-25 19:35:43 -0400

    added marker to maps

--- app/assets/javascripts/map_load.js
+++ app/assets/javascripts/map_load.js
@@ -1,31 +1,21 @@
 function initMap() {
-  lat = $('#latitude_field').val();
-  long = $('#longitude_field').val();
+  lat = parseFloat($('#latitude_field').val());
+  long = parseFloat($('#longitude_field').val());
   map = new google.maps.Map(document.getElementById('map-canvas'), {
     center: new google.maps.LatLng(lat, long),
     zoom: 15,
     mapTypeId: google.maps.MapTypeId.ROADMAP
   });
+  createMarker(lat, long, map);
 }
 
-// function createMarker(lat, long, map) {
-//   placeLoc = {};
-//   placeLoc.lat = lat;
-//   placeLoc.lng = long;
-//   marker = new google.maps.Marker({
-//     map: map,
-//     animation: google.maps.Animation.DROP,
-//     position: placeLoc
-//   });
-// }
-//
-// createMarker(lat, long, map);
-
-//
-// lat = $('#latitude_field').val();
-// long = $('#longitude_field').val();
-// map = new google.maps.Map(document.getElementById('map-canvas'), {
-//   center: new google.maps.LatLng(lat, long),
-//   zoom: 15,
-//   mapTypeId: google.maps.MapTypeId.ROADMAP
-// });
+function createMarker(lat, long, map) {
+  placeLoc = {};
+  placeLoc.lat = lat;
+  placeLoc.lng = long;
+  marker = new google.maps.Marker({
+    map: map,
+    animation: google.maps.Animation.DROP,
+    position: placeLoc
+  });
+}
