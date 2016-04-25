6e573f1acb4ee1e778fcfeffd1b8bd205c94a852
Anthony Degennaro <anthonydegennaro@Anthonys-iMac.fios-router.home>
2016-04-24 21:51:37 -0400

    edited variables

--- app/assets/javascripts/map_load.js
+++ app/assets/javascripts/map_load.js
@@ -41,7 +41,7 @@
 function initMap() {
   lat = $('#latitude_field').val();
   long = $('#longitude_field').val();
-  map = new google.maps.Map(document.getElementById('map'), {
+  map = new google.maps.Map(document.getElementById('map-canvas'), {
     center: new google.maps.LatLng(lat, long),
     zoom: 15,
     mapTypeId: google.maps.MapTypeId.ROADMAP
