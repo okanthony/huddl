1f475f22322c3e6f134e5ac3560ffdeb74a5e8bc
Anthony Degennaro <anthonydegennaro@Anthonys-iMac.fios-router.home>
2016-05-01 22:02:43 -0400

    email variables

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -12,10 +12,10 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = "admin@huddl.herokuapp.com"
+  config.mailer_sender = "app50126369@heroku.com"
 
   # Configure the class responsible to send e-mails.
-  config.mailer = 'ActionMailer::Base'
+  # config.mailer = 'Devise::Mailer'
 
   # ==> ORM configuration
   # Load and configure the ORM. Supports :active_record (default) and
