1577a6b21078b3c2ade7f7b67b6fce195fb1f74b
Anthony Degennaro <anthonydegennaro@Anthonys-iMac.fios-router.home>
2016-05-01 21:55:24 -0400

    specified ActionMailer devise.rb

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -15,7 +15,7 @@ Devise.setup do |config|
   config.mailer_sender = "admin@huddl.herokuapp.com"
 
   # Configure the class responsible to send e-mails.
-  # config.mailer = 'Devise::Mailer'
+  config.mailer = 'ActionMailer::Base'
 
   # ==> ORM configuration
   # Load and configure the ORM. Supports :active_record (default) and
