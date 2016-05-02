056b7b4d0361af32b5cedcf0c35d5cde10ef5cf8
Anthony Degennaro <anthonydegennaro@Anthonys-iMac.fios-router.home>
2016-05-02 01:45:47 -0400

    hound issues

--- app/assets/javascripts/rosterclaim.js
+++ app/assets/javascripts/rosterclaim.js
@@ -4,28 +4,27 @@ $("#roster-button").on("submit", function(event) {
 var url = $(this).attr("href");
 var roster = $(this).parent().find("#roster-list");
 
-if ($("#actual-button")[0].value === "Claim") {
-  var request = $.ajax({
-    type: "POST",
-    url: url,
-    dataType: "json",
-    success: function(response) {
-      var player = document.createElement("li");
-      player.appendChild(document.createTextNode(response.name.first_name + ' ' + response.name.last_name));
-      roster[0].appendChild(player);
-      document.getElementById("actual-button").value = response.button_name;
-    }
-  });
-} else {
-  var request = $.ajax({
-    type: "POST",
-    url: url,
-    dataType: "json",
-    success: function(response) {
-      $('#roster-list li:last-child')[0].remove();
-      document.getElementById("actual-button").value = response.button_name;
-    }
-  });
-}
-
+  if ($("#actual-button")[0].value === "Claim") {
+    var request = $.ajax({
+      type: "POST",
+      url: url,
+      dataType: "json",
+      success: function(response) {
+        var player = document.createElement("li");
+        player.appendChild(document.createTextNode(response.name.first_name + ' ' + response.name.last_name));
+        roster[0].appendChild(player);
+        document.getElementById("actual-button").value = response.button_name;
+      }
+    });
+  } else {
+    var request = $.ajax({
+      type: "POST",
+      url: url,
+      dataType: "json",
+      success: function(response) {
+        $('#roster-list li:last-child')[0].remove();
+        document.getElementById("actual-button").value = response.button_name;
+      }
+    });
+  }
 });
