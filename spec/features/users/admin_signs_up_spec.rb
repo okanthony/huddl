ae487d1df52e7c2df6c098a5ed971db1bdd8b296
Anthony Degennaro <anthonydegennaro@Anthonys-iMac.fios-router.home>
2016-05-01 20:44:31 -0400

    fixed create team tests

--- spec/features/users/admin_signs_up_spec.rb
+++ spec/features/users/admin_signs_up_spec.rb
@@ -1,14 +1,12 @@
 require "rails_helper"
 
 feature "admin signs up" do
-  let!(:team1) { FactoryGirl.create(:team) }
-  let!(:captain1) { FactoryGirl.create(:user, admin: true, team: team1) }
   scenario "specifying valid and required information" do
     visit unauthenticated_root_path
     click_link "Sign Up"
     fill_in "First Name", with: "John"
     fill_in "Last Name", with: "Smith"
-    fill_in "Email", with: "user@example.com"
+    fill_in "Email", with: "user98@example.com"
     fill_in "Password", with: "password"
     fill_in "Password Confirmation", with: "password"
     click_button "Sign Up"
@@ -26,7 +24,7 @@ feature "admin signs up" do
     expect(page).to have_content("Add Game")
   end
 
-  scenario "required information not specified" do
+  scenario "required account information not specified" do
     visit unauthenticated_root_path
     click_link "Sign Up"
     click_button "Sign Up"
@@ -45,4 +43,21 @@ feature "admin signs up" do
     expect(page).to have_content("doesn't match")
     expect(page).to_not have_content("Sign Out")
   end
+
+  scenario "required team information not specified" do
+    visit unauthenticated_root_path
+    click_link "Sign Up"
+    fill_in "First Name", with: "John"
+    fill_in "Last Name", with: "Smith"
+    fill_in "Email", with: "user99@example.com"
+    fill_in "Password", with: "password"
+    fill_in "Password Confirmation", with: "password"
+    click_button "Sign Up"
+    select "Baseball", from: "Sport"
+    click_button "Create Team"
+
+    expect(page).to have_content("can't be blank")
+    expect(page).to have_content("Create Your Team")
+  end
+
 end
