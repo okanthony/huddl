9fd6424405ae1832d5be2d4ad0e73f5318309b6b
Anthony Degennaro <anthonydegennaro@Anthonys-iMac.fios-router.home>
2016-05-12 22:15:36 -0400

    changed test mailer settings

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -39,9 +39,10 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-  config.action_mailer.default_url_options = {:host => "localhost:3000"}
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { :host => "http://huddl.herokuapp.com" }
 end
 
 # Rails.application.routes.default_url_options[:host] = 'localhost'
