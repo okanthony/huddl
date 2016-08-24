064ee714a63e563d645252a0adbb6c41fcc49f63
Anthony Degennaro <anthonydegennaro@Anthonys-iMac.fios-router.home>
2016-08-23 23:02:52 -0400

    refactored js

--- app/assets/javascripts/roster_claim.js
+++ app/assets/javascripts/roster_claim.js
@@ -1,31 +1,26 @@
 $("#roster-button").on("submit", function(event) {
   event.preventDefault();
 
-var url = $(this).attr("href");
-var roster = $(this).parent().find("#roster-list");
-  if ($("#actual-button")[0].value === "Claim") {
-    var request = $.ajax({
-      type: "POST",
-      url: url,
-      dataType: "json",
-      success: function(response) {
-        var full_name = response.name.first_name + ' ' + response.name.last_name;
-        var player = document.createElement("li");
-        player.setAttribute("id", full_name);
-        player.appendChild(document.createTextNode(full_name));
-        roster[0].appendChild(player);
-        document.getElementById("actual-button").value = response.button_name;
+  var url = $(this).attr("href");
+  var roster = $(this).parent().find("#roster-list");
+  $.ajax({
+    type: "POST",
+    url: url,
+    dataType: "json",
+    success: function(response) {
+      var id_name = response.name.first_name + '-' + response.name.last_name;
+      var full_name = response.name.first_name + ' ' + response.name.last_name;
+      if ($("#actual-button").val() === "Claim") {
+        var $player = $("<li>", {
+          id: id_name,
+          text: full_name
+        });
+        roster.append($player);
+        $("#actual-button").val(response.button_name);
+      } else {
+        $("#" + id_name).remove();
+        $("#actual-button").val(response.button_name);
       }
-    });
-  } else {
-    var request = $.ajax({
-      type: "POST",
-      url: url,
-      dataType: "json",
-      success: function(response) {
-        document.getElementById(response.name.first_name + ' ' + response.name.last_name).remove();
-        document.getElementById("actual-button").value = response.button_name;
-      }
-    });
-  }
+    }
+  });
 });
